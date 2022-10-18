3f2029ccd4910f95c98465dd8f0b278e63654a34
Benakich <Benakich@gmail.com>
2022-10-18 13:54:14 +0000

    putchar and print char

--- print_functions.c
+++ print_functions.c
@@ -1,12 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * print_char - fn to print char
- * @arg: 
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _print_char - fn to print char
+ * @args: char argument passed
  */
 
-void (*_print_char)(va_list args)
+void _print_char(va_list args)
 {
 	return (_putchar(va_arg(args, int)));
 }
