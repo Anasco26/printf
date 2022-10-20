588788bd6b6576bec57834c58aff13f44663b1b8
Ishaq <umarishaq26@gmail.com>
2022-10-20 05:43:45 -0700

    Main.h updated

--- main.h
+++ main.h
@@ -6,6 +6,8 @@
 
 #define BUFF_SIZE 1024
 #define UNUSED(x) (void)(x)
+#define S_LONG 2
+#define S_SHORT 1
 
 /**
  * struct fmt - struct to choose the right function depending
@@ -16,18 +18,54 @@
 typedef struct fmt
 {
 	char fmt;
-	int (*fn)(va_list);
+	int (*fn)(va_list, char[], int, int, int, int);
 } fmt_t;
 
+/*The Printf function */
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *ind, va_list list);
+
+/* The Handler functions*/
+int handle_print(const char *fmt, int *i,
+		va_list list, char buffer[], int flags, int width, int precision, int size);
+
+int handle_write_char(char c, char buffer[],
+		int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+		int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+		int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+		int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+		char buffer[],
+		int flags, int width, int precision, int size);
+
+/* The library functions */
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 int _putchar(char c);
-int print_char(va_list args);
-int print_str(va_list args);
-int print_percent(va_list args);
-int print_unsigned(va_list args);
 
+/* Help functions */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
-#endif
+/* Printf specifiers functions */
+int print_char(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_str(va_list args, char buffer[],
+		int flags, int width, int precision, int size);
+int print_percent(va_list args, char buffer[],
+		int flags, int width, int precision, int size);
+int print_unsigned(va_list args, char buffer[],
+		int flags, int width, int precision, int size);
+
+#endif /* MAIN._H */
 
